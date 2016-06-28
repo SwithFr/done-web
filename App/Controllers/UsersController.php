f6f893709f15f1c4649b95c72a4364fe5f3346e2
SwithFr <contact@swith.fr>
2016-06-28 14:47:54 +0200

    register new user action

--- App/Controllers/UsersController.php
+++ App/Controllers/UsersController.php
@@ -4,6 +4,8 @@
 namespace App\Controllers;
 
 
+use App\Models\User;
+
 class UsersController extends AppController
 {
     public function connect()
@@ -18,6 +20,20 @@ class UsersController extends AppController
 
     public function register()
     {
-        
+        if ($this->Request->isPost) {
+            $user = new User();
+
+            $data = $this->Request->data;
+            if ($user->validate($data)) {
+                $user->data = $data;
+                $user->store->connect();
+                $this->Session->write("usertoken", $user->recived_data->data->user_token);
+                $this->Session->write("userid", $user->recived_data->data->user_id);
+                $this->Session->setFlash("Votre compte a bien été crée ! Vous êtes maintenant connecté.");
+                $this->redirect("tableau-de-bord");
+            } else {
+                $this->Session->setFlash("Désolé mais vos informations ne sont pas valides", 'error');
+            }
+        }
     }
 }
