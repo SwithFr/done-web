91db76c84c2573ead5a65609cd8f1e9855a7848e
SwithFr <contact@swith.fr>
2016-06-28 12:22:42 +0200

    typo

--- App/Views/Elements/add_project_form.php
+++ App/Views/Elements/add_project_form.php
@@ -2,7 +2,7 @@
     $nameError = isset($_SESSION['project-errors']['name']) ? 'hasError' : '';
     unset($_SESSION['project-errors']);
 ?>
-<form class="form" action="<?= \Core\Helpers\Html::url("decouvrir/projects") ?>" method="post">
+<form class="form" action="<?= \Core\Helpers\Html::url("decouvrir/projets") ?>" method="post">
     <label class="<?= $nameError ?>" for="name">Nom du projet</label>
     <input type="text" class="form__input <?= $nameError ?>" name="name" id="name" placeholder="Courses">
     <input class="button button--smaller form__input form__submit" type="submit" value="Ajouter">
