96841b9f0ca547f53d3ed2fd0aa0a60e44592287
SwithFr <contact@swith.fr>
2016-06-28 12:22:08 +0200

    add UsersController

---
+++ App/Controllers/UsersController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+class UsersController extends AppController
+{
+    public function connect()
+    {
+        
+    }
+
+    public function logout()
+    {
+        
+    }
+
+    public function register()
+    {
+        
+    }
+}
