84a2f2c5715009df98f732a4f3719177caf0baab
SwithFr <contact@swith.fr>
2016-06-29 15:19:55 +0200

    make dashboard dynamic

--- App/Views/Pages/user_dashboard.php
+++ App/Views/Pages/user_dashboard.php
@@ -2,52 +2,33 @@
 <?= $this->element("search_task_form") ?>
 <?= $this->element("add_buttons") ?>
 <h1><span class="hidden">Vos tâches pour </span>Aujourd'hui</h1>
-<ul class="projects">
-    <li class="project">
-        <h2 style="color:red;">Titre du projet</h2>
-        <div class="actions">
-            <a href="" title="Editer le projet">
-                <svg width="15" height="15" role="img" class="edit">
-                    <use xlink:href="#edit"></use>
-                </svg>
-            </a>
-            <a href="" title="Marquer le projet comme accomplie">
-                <svg width="15" height="15" role="img" class="done">
-                    <use xlink:href="#done"></use>
-                </svg>
-            </a>
-            <a href="" title="Supprimer le projet">
-                <svg width="15" height="15" role="img" class="suppr">
-                    <use xlink:href="#suppr"></use>
-                </svg>
-            </a>
-        </div>
-        <ul class="project__tasks">
-            <?php for($i = 0; $i < 3; $i++): ?>
-                <li class="task">
-                    <h3 style="color:blue;">Titre de la tâche</h3>
-                    <p class="task_note">
-                        <?= Text::cut("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi hic mollitia odit officia perspiciatis qui reprehenderit? Autem deleniti deserunt eius facilis fugiat magni neque obcaecati, perferendis provident quam, sequi, vel!", 100) ?>
-                    </p>
-                    <div class="actions">
-                        <a href="" title="Editer la tâche">
-                            <svg width="15" height="15" role="img" class="edit">
-                                <use xlink:href="#edit"></use>
-                            </svg>
-                        </a>
-                        <a href="" title="Marquer la tâche comme accomplie">
-                            <svg width="15" height="15" role="img" class="done">
-                                <use xlink:href="#done"></use>
-                            </svg>
-                        </a>
-                        <a href="" title="Supprimer la tâche">
-                            <svg width="15" height="15" role="img" class="suppr">
-                                <use xlink:href="#suppr"></use>
-                            </svg>
-                        </a>
-                    </div>
-                </li>
-            <?php endfor; ?>
-        </ul>
-    </li>
-</ul>
+<?php if(!empty($projects)): ?>
+    <ul class="projects">
+        <?php foreach($projects as $project): ?>
+            <li class="project">
+                <h2 style="color:red;"><?= $project->name ?></h2>
+                <?= $this->element('actions', ['type' => 'projet', 'item' => $project]) ?>
+                <ul class="project__tasks">
+                    <?php if(!empty($project->tasks)): ?>
+                        <?php foreach($project->tasks as $task): ?>
+                            <li class="task">
+                                <h3 style="color:blue;"><?= $task->title ?></h3>
+                                <?php if(isset($task->note)): ?>
+                                    <p class="task_note">
+                                        <?= Text::cut($task->note, 100) ?>
+                                    </p>
+                                <?php endif; ?>
+                                <?= $this->element('actions', ['type' => 'tache', 'item' => $task]) ?>
+                            </li>
+                        <?php endforeach; ?>
+                    <?php else: ?>
+                        Pas encore de tâche ! Ajoutez-en une.
+                    <?php endif; ?>
+                </ul>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php else: ?>
+    Pas encore de projet ! Ajoutez-en un.
+<?php endif; ?>
+
