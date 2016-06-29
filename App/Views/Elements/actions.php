1b2f7c4a04e68eb954cd9ca44ae5821339adbd57
SwithFr <contact@swith.fr>
2016-06-29 15:19:41 +0200

    add actions elements

---
+++ App/Views/Elements/actions.php
@@ -0,0 +1,25 @@
+<?php use Core\Helpers\Html; ?>
+<?php
+    if ($type == "tache") {
+        $label = "la tâche";
+    } else {
+        $label = "le projet";
+    }
+?>
+<div class="actions">
+    <a href="<?= Html::url("$type/edition/{$item->id}") ?>" title="Editer <?= $label ?>">
+        <svg width="15" height="15" role="img" class="edit">
+            <use xlink:href="#edit"></use>
+        </svg>
+    </a>
+    <a href="<?= Html::url("$type/done/{$item->id}") ?>" title="Marquer <?= $label ?> comme accomplie">
+        <svg width="15" height="15" role="img" class="done">
+            <use xlink:href="#done"></use>
+        </svg>
+    </a>
+    <a href="<?= Html::url("$type/suppression/{$item->id}") ?>" title="Supprimer <?= $label ?>">
+        <svg width="15" height="15" role="img" class="suppr">
+            <use xlink:href="#suppr"></use>
+        </svg>
+    </a>
+</div>
