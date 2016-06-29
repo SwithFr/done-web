28efb67ed52ce14e81bb32953fbc24ed7f9bc1e1
SwithFr <contact@swith.fr>
2016-06-29 15:18:34 +0200

    get data for dashboard

--- App/Controllers/PagesController.php
+++ App/Controllers/PagesController.php
@@ -3,6 +3,9 @@
 namespace App\Controllers;
 
 
+use App\Models\Project;
+use App\Models\Task;
+
 class PagesController extends AppController
 {
 
@@ -14,7 +17,22 @@ class PagesController extends AppController
 
     public function user_dashboard()
     {
-        var_dump("cool");die();
+        // Récup projects
+        $projectModel = new Project();
+        $taskModel = new Task();
+        $d['projects'] = $projectModel->getAll()->recived_data->data;
+
+        // récup taches
+        if ($d['projects']) {
+            foreach ($d['projects'] as $project) {
+                $project->tasks = $taskModel->getAll([
+                    'route' => $project->id
+                ])->recived_data->data;
+            }
+            $this->Session->write("hasProject", true);
+        }
+
+        return $this->set($d);
     }
 
 }
