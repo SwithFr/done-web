96a6ac7a5bd4527a2948dc77b20ff8e921ad08cb
SwithFr <contact@swith.fr>
2016-07-16 16:54:36 +0200

    disable link

--- App/Views/Elements/add_buttons.php
+++ App/Views/Elements/add_buttons.php
@@ -1,2 +1,2 @@
-<a class="add-button <?= isset($_SESSION['hasProject']) ? '' : 'disabled' ?>" href="<?= Html::url("taches/ajout") ?>">Tâche</a>
+<a class="add-button <?= isset($_SESSION['hasProject']) ? '' : 'disabled' ?>" href="<?= isset($_SESSION['hasProject']) ?  Html::url("taches/ajout")  : '#' ?>">Tâche</a>
 <a class="add-button" href="<?= Html::url("projets/ajout") ?>">Projet</a>
