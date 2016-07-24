3f3e44e989a5b9ad04a97ca1a1ef8f61417f2f04
SwithFr <contact@swith.fr>
2016-07-24 10:51:51 +0200

    add back link

--- App/Views/Projects/user_add.php
+++ App/Views/Projects/user_add.php
@@ -1,4 +1,5 @@
 <?php $title_for_layout = "Ajouter un projet"; ?>
+<a class="back-link" title="retour au tableau de bord" href="<?= Html::url('tableau-de-bord') ?>">Retour au tableau de bord</a>
 <section class="section">
     <h2 class="section__title">Ajouter un projet</h2>
     <p class="section__content">
--- App/Views/Tasks/user_add.php
+++ App/Views/Tasks/user_add.php
@@ -1,4 +1,5 @@
 <?php $title_for_layout = "Ajouter une tâche"; ?>
+<a class="back-link" title="retour au tableau de bord" href="<?= Html::url('tableau-de-bord') ?>">Retour au tableau de bord</a>
 <section class="section">
     <h2 class="section__title">Ajouter une tâche</h2>
 </section>
