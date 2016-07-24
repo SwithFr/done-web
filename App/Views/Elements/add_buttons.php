120e4007680209c4cfde9bb282e8be665a21e4bd
SwithFr <contact@swith.fr>
2016-07-24 09:57:18 +0200

    buttons css

--- App/Views/Elements/add_buttons.php
+++ App/Views/Elements/add_buttons.php
@@ -1,5 +1,11 @@
 <div class="buttons-container">
     <a class="add-button <?= isset($_SESSION['hasProject']) ? '' : 'disabled' ?>" href="<?= isset($_SESSION['hasProject']) ?  Html::url("taches/ajout")  : '#' ?>">Tâche</a>
     <a class="add-button" href="<?= Html::url("projets/ajout") ?>">Projet</a>
-    <a class="add-button" href="">Options</a>
+    <div class="button-wrapper">
+        <a class="add-button" href="#">Options</a>
+        <span class="add-button__content">
+            <a class="add-button" href="<?= Html::url("etat/ajout") ?>">État</a>
+            <a class="add-button" href="<?= Html::url("tag/ajout") ?>">Tag</a>
+        </span>
+    </div>
 </div>
